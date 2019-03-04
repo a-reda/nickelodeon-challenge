3905f59cf4b02490b0c93183f2a2d9bc1dec9bef
Reda Aissaoui <reda@aissaoui.org>
2019-03-04 22:47:18 +0100

    grid resp WIP

--- src/components/Program.js
+++ src/components/Program.js
@@ -14,15 +14,11 @@ class Program extends Component {
     return (
       <div className="Program">
         <div className="Programinfo">
-            <div className="Leftinfo">
-              <div className="Thumbnail"><img src={p.images['r16-9']}/></div>
-              <div className="Programtext">
-                  {this.getFormattedTime(p)}
-                  <h3 className="Programtitle">{p.seriesTitle}</h3>
-                  <h3 className="Episodetitle">{p.episodeTitle}</h3>
-              </div>
-            </div>
-            <div className="Toggleiconcontainer">{this.getToggleIcon()}</div>
+          <div className="Thumbnail"><img src={p.images['r16-9']}/></div>
+          <div className="Programtime">{this.getFormattedTime(p)}</div>
+          <div className="Programtitle"><h3>{p.seriesTitle}</h3></div>
+          <div><h3 className="Episodetitle">{p.episodeTitle}</h3></div>
+          <div className="Toggleiconcontainer">{this.getToggleIcon()}</div>
         </div>
         {this.state.showDescription ?
           <div className="Programdescr">
@@ -44,9 +40,7 @@ class Program extends Component {
 
   getFormattedTime = (p) => {
     const className = p._onnow ? 'Onnow': p._upnext  ? 'Upnext' : 'Timetitle';
-    return (<div className='Timerow'>
-              <h3 className={className}>{p.formattedTime}</h3>
-            </div>)
+    return (<h3 className={className}>{p.formattedTime}</h3>)
   }
 
 }
